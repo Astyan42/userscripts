7ab4e001fa1aa50576c80b04fcf86a83c60d373f
Tuan Nghia Duong <dtnghia90@gmail.com>
2014-11-07 15:50:32 +0100

    Wait for the page to load before doing anything

--- spiraCommentaire.js
+++ spiraCommentaire.js
@@ -14,10 +14,19 @@
 // @copyright  2012+, You
 // ==/UserScript==
 
-var tdButton = document.createElement("td");
-var button = document.createElement("button");
-button.innerHTML = 'pré-remplir';
-tdButton.setAttribute("width","70px");
-tdButton.appendChild(button);
-button.setAttribute("onclick","var templateCommentaire = '<ul><li> <b>Corrigé</b></li><li> <b>Problème :</b> <i> obligatoire, détailler...</i> </li><li> <b>Solution :</b> <i>obligatoire, détailler...</i></li><li> <b>Tests :</b> <i>obligatoire, détailler...</i></li><li> <b>Impacts potentiels :</b> <i>obligatoire, détailler...</i> </li><li> <b>@QA (Non Reg.) :</b> <i>obligatoire, détailler...</i></li><li> <b>@Support :</b> <i>optionnel</i></li><li> <b>Origine :</b> <i>optionnel cause de la régression</i> (pour la maintenance)</li></ul>';	document.querySelector('#cplMainContent_pnlOverview_Comments .rteDiv').innerHTML = templateCommentaire;return false;");
-document.querySelector("#cplMainContent_pnlOverview_Comments .rteBack tr").appendChild(tdButton);
+// Wait for the page to load before anything
+document.onreadystatechange = function () 
+{
+	if (document.readyState == "complete") 
+	{
+		var tdButton = document.createElement("td");
+		var button = document.createElement("button");
+		button.innerHTML = 'Pré-remplir';
+		tdButton.setAttribute("width","70px");
+		tdButton.appendChild(button);
+		button.setAttribute(
+			"onclick",
+			"var templateCommentaire = '<ul><li> <b>Corrigé</b></li><li> <b>Problème :</b> <i> obligatoire, détailler...</i> </li><li> <b>Solution :</b> <i>obligatoire, détailler...</i></li><li> <b>Tests :</b> <i>obligatoire, détailler...</i></li><li> <b>Impacts potentiels :</b> <i>obligatoire, détailler...</i> </li><li> <b>@QA (Non Reg.) :</b> <i>obligatoire, détailler...</i></li><li> <b>@Support :</b> <i>optionnel</i></li><li> <b>Origine :</b> <i>optionnel cause de la régression</i> (pour la maintenance)</li></ul>';	document.querySelector('#cplMainContent_pnlOverview_Comments .rteDiv').innerHTML = templateCommentaire;return false;");
+		document.querySelector("#cplMainContent_pnlOverview_Comments .rteBack tr").appendChild(tdButton);
+	}
+}
