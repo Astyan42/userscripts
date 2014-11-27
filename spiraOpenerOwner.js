2d220f5692aefcd14b1a90f1b2f6a9150ab94597
Aymeric Poude <aymeric.poude@crossknowledge.com>
2014-11-27 13:39:31 +0100

    Update spiraOpenerOwner.js (added a button for PHP team)

--- spiraOpenerOwner.js
+++ spiraOpenerOwner.js
@@ -35,7 +35,7 @@ function createButton(dropDownId)
 {
     var referenceNode = document.querySelector("#" + dropDownId);
     
-    // Create QA-Team button
+    // Create QA-Team and a PHP-Team button
     if (dropDownId == 'cplMainContent_ddlOwner') {
         var qaButton = document.createElement("button");
         qaButton.innerHTML = "QA-Team";
@@ -43,6 +43,14 @@ function createButton(dropDownId)
         
         qaButton.onclick = function() {
             return buttonOnCLick(dropDownId, "QA- Team");
+        }
+
+        var phpButton = document.createElement("button");
+        phpButton.innerHTML = "PHP-Team";
+        referenceNode.parentNode.insertBefore(phpButton, referenceNode.nextSibling);
+        
+        phpButton.onclick = function() {
+            return buttonOnCLick(dropDownId, "PHP- Team");
         }
     }
 
